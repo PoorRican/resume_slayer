e11b86882bac44e16aac340768401be001d694d0
Josue Figueroa <poor.rican@pm.me>
2023-08-09 17:31:33 -0700

    Added new chains for improving resume section

--- util.py
+++ util.py
@@ -2,7 +2,7 @@ from enum import Enum
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import Document
-from langchain.output_parsers import PydanticOutputParser, EnumOutputParser
+from langchain.output_parsers import PydanticOutputParser, EnumOutputParser, CommaSeparatedListOutputParser
 from langchain.chains import LLMChain, SequentialChain
 from langchain.text_splitter import MarkdownHeaderTextSplitter
 from pydantic import BaseModel
@@ -67,6 +67,62 @@ def cut_sections(resume: str) -> dict:
         resume_model[key]['result'] = classify_chain.predict(resume=resume,
                                                              section_name=resume_model[key]['section_name'])
     return resume_model
+
+
+def get_key_skill() -> LLMChain:
+    """ Extract skills relevant to job description from resume section """
+    output_parser = CommaSeparatedListOutputParser()
+    format_instructions = output_parser.get_format_instructions()
+
+    prompt = PromptTemplate(template="""
+    You're an expert career consultant with an IQ over 140 working with a special client regarding this job posting.
+    You will be given a list of required skills, and an excerpt from the client's resume.
+    What required 3 skills does the client have?
+    
+    Resume excerpt:\n\t{section}
+    
+    Required skills:\n\t{requirements}
+    
+    {format_instructions}
+    """,
+                            input_variables=['section', 'requirements'],
+                            partial_variables={'format_instructions': format_instructions}
+                            )
+    return LLMChain(prompt=prompt,
+                    llm=ChatOpenAI(temperature=.1,
+                                   model_name="gpt-3.5-turbo"),
+                    output_parser=output_parser)
+
+
+def _summary_sentence_from_skills() -> LLMChain:
+    """ Generate a summary sentence showing how a job experience is demonstrates the given skills.
+
+    This produces a nicely worded, concise summary. This might be useful in the future,
+    but is not planned in be implemented now.
+    """
+    prompt = PromptTemplate.from_template("""
+    You will be given a section of a resume and 3 key skills.
+    Please write a one sentence summary for the resume section highlighting some or all of the given skills.
+    
+    Skills: {skills}
+    
+    Section: {section} 
+    """)
+    return LLMChain(prompt=prompt, llm=ChatOpenAI(temperature=.1, model_name="gpt-3.5-turbo"))
+
+
+def highlight_chain() -> LLMChain:
+    """ Frame weak job experience section to highlight key skills """
+
+    prompt = PromptTemplate.from_template("""
+    You will be given a job experience from a resume and 3 key skills.
+    Use bullet points to highlight how the job experience section demonstrates the given skills.
+    
+    Skills: {skills}
+    
+    Job Experience: {section} 
+    """)
+    return LLMChain(prompt=prompt, llm=ChatOpenAI(temperature=.1, model_name="gpt-3.5-turbo"))
 
 
 def process_history_chain() -> SequentialChain:
