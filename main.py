77bbf3c6fa55e2b068a416a04efda1ba25bc66ed
Josue Figueroa <poor.rican@pm.me>
2023-08-30 02:40:09 -0700

    Add CORS middleware

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from slayer import Slayer
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Replace with the origin of your React client
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class ResumeRequest(BaseModel):
