df171a4d92d7a7db97fb359a1280172504baeb69
Josue Figueroa <poor.rican@pm.me>
2023-09-13 12:12:19 -0700

    Add more secure allowed origin

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-                   '*'],
+                   'https://resume-slayer-*-poorrican.vercel.app'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
